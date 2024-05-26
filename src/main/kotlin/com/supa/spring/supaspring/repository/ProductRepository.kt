979c5468b8bcdfe12077e47c01888abf068320be
Hieu Vu <star.hieu.99@gmail.com>
2024-05-26 09:36:05 +0700

    Update supabase client

--- src/main/kotlin/com/supa/spring/supaspring/repository/ProductRepository.kt
+++ src/main/kotlin/com/supa/spring/supaspring/repository/ProductRepository.kt
@@ -2,27 +2,13 @@ package com.supa.spring.supaspring.repository
 
 import com.supa.spring.supaspring.controller.dto.ProductDto
 import io.github.jan.supabase.SupabaseClient
-import io.github.jan.supabase.createSupabaseClient
-import io.github.jan.supabase.gotrue.Auth
 import io.github.jan.supabase.postgrest.Postgrest
 import io.github.jan.supabase.postgrest.postgrest
-import io.github.jan.supabase.serializer.JacksonSerializer
 import kotlinx.coroutines.runBlocking
 import org.springframework.stereotype.Repository
 
 @Repository
-
-class ProductRepository constructor
-    (
-    supabase: SupabaseClient = createSupabaseClient(
-        supabaseUrl = "https://tyjxzodfrahibdkhgquy.supabase.co",
-        supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InR5anh6b2RmcmFoaWJka2hncXV5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODUxMTM2MDcsImV4cCI6MjAwMDY4OTYwN30.rXN-_rGEHKjOlo75xoDYOPkshrno9rK7h1H-CXFb2_g"
-    ) {
-        install(Auth)
-        install(Postgrest)
-        defaultSerializer = JacksonSerializer()
-    }
-) {
+class ProductRepository(supabase: SupabaseClient) {
 
     val postgrest: Postgrest = supabase.postgrest
 
