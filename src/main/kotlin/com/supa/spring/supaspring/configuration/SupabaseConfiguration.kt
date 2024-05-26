16b3951f6394466cab6fff079aabc396060811fb
Hieu Vu <star.hieu.99@gmail.com>
2024-05-26 09:32:42 +0700

    Provide supabase client

---
+++ src/main/kotlin/com/supa/spring/supaspring/configuration/SupabaseConfiguration.kt
@@ -0,0 +1,24 @@
+package com.supa.spring.supaspring.configuration
+
+import io.github.jan.supabase.SupabaseClient
+import io.github.jan.supabase.createSupabaseClient
+import io.github.jan.supabase.gotrue.Auth
+import io.github.jan.supabase.postgrest.Postgrest
+import io.github.jan.supabase.serializer.JacksonSerializer
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+
+@Configuration
+class SupabaseConfiguration {
+    @Bean
+    fun supabaseClient(): SupabaseClient {
+        return createSupabaseClient(
+            supabaseUrl = "https://tyjxzodfrahibdkhgquy.supabase.co",
+            supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InR5anh6b2RmcmFoaWJka2hncXV5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODUxMTM2MDcsImV4cCI6MjAwMDY4OTYwN30.rXN-_rGEHKjOlo75xoDYOPkshrno9rK7h1H-CXFb2_g"
+        ) {
+            install(Auth)
+            install(Postgrest)
+            defaultSerializer = JacksonSerializer()
+        }
+    }
+}
