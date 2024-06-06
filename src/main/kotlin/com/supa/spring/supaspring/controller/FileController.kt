026fac53237052c14a625249deffcb304d40f142
Hieu Vu <star.hieu.99@gmail.com>
2024-06-06 22:08:08 +0700

    Implement retrieve all files in bucket

--- src/main/kotlin/com/supa/spring/supaspring/controller/FileController.kt
+++ src/main/kotlin/com/supa/spring/supaspring/controller/FileController.kt
@@ -1,6 +1,7 @@
 package com.supa.spring.supaspring.controller
 
 import com.supa.spring.supaspring.controller.dto.BucketDto
+import com.supa.spring.supaspring.controller.dto.BucketItemDto
 import com.supa.spring.supaspring.service.FileService
 import org.springframework.http.HttpStatusCode
 import org.springframework.http.ResponseEntity
@@ -14,21 +15,18 @@ class FileController(val fileService: FileService) {
     @GetMapping("/{bucketId}")
     fun getFiles(
         @PathVariable bucketId: String
-    ): ResponseEntity<List<BucketDto>> {
+    ): ResponseEntity<List<BucketItemDto>> {
         val result = fileService.getFilesInBucket(bucketId)
-        return ResponseEntity<List<BucketDto>>(Unit, HttpStatusCode.valueOf(200))
+        return ResponseEntity<List<BucketItemDto>>(result, HttpStatusCode.valueOf(200))
     }
 
     @PutMapping
     fun moveFile(
         @RequestBody from: String,
         @RequestBody to: String
-    ): ResponseEntity<BucketDto> {
-        val result = fileService.moveFile(from, to)
-        return if (result == null) ResponseEntity<BucketDto>(
-            null,
-            HttpStatusCode.valueOf(400)
-        ) else ResponseEntity<BucketDto>(result, HttpStatusCode.valueOf(200))
+    ): ResponseEntity<Unit> {
+        fileService.moveFile(from, to)
+        return ResponseEntity(Unit, HttpStatusCode.valueOf(200))
     }
 
     @PostMapping("/{bucketId}")
--- src/main/kotlin/com/supa/spring/supaspring/service/FileService.kt
+++ src/main/kotlin/com/supa/spring/supaspring/service/FileService.kt
@@ -1,13 +1,15 @@
 package com.supa.spring.supaspring.service
 
+import com.supa.spring.supaspring.controller.dto.BucketItemDto
+import com.supa.spring.supaspring.repository.FileRepository
 import org.springframework.stereotype.Service
 import org.springframework.util.MultiValueMap
 import org.springframework.web.multipart.MultipartFile
 
 @Service
-class FileService {
-    fun getFilesInBucket(bucketId: String) {
-
+class FileService(val fileRepository: FileRepository) {
+    fun getFilesInBucket(bucketId: String): List<BucketItemDto> {
+        return fileRepository.getFilesInBucket(bucketId)
     }
 
     fun moveFile(from: String, to: String) {
