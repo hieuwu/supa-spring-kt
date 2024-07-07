155c9b5efc2e44e0b086aa73f743ff9561d83da5
Hieu Vu <star.hieu.99@gmail.com>
2024-07-07 22:10:07 +0700

    Implement auth service

--- src/main/kotlin/com/supa/spring/supaspring/auth/domain/AuthService.kt
+++ src/main/kotlin/com/supa/spring/supaspring/auth/domain/AuthService.kt
@@ -1,4 +1,6 @@
 package com.supa.spring.supaspring.auth.domain
 
 interface AuthService {
+    fun signUp(email: String, password: String)
+    fun signIn(email: String, password: String)
 }
--- src/main/kotlin/com/supa/spring/supaspring/auth/domain/SupabaseAuthService.kt
+++ src/main/kotlin/com/supa/spring/supaspring/auth/domain/SupabaseAuthService.kt
@@ -1,4 +1,14 @@
 package com.supa.spring.supaspring.auth.domain
 
-class SupabaseAuthService {
+import org.springframework.stereotype.Service
+
+@Service
+class SupabaseAuthService constructor(val authRepository: AuthRepository) : AuthService {
+    override fun signUp(email: String, password: String) {
+        TODO("Not yet implemented")
+    }
+
+    override fun signIn(email: String, password: String) {
+        TODO("Not yet implemented")
+    }
 }
